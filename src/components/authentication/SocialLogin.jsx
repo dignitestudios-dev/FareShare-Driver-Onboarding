7d1a9f5c74c8ce36a04b046c7d1fa1c7f9c08953
Hammad ul haq <120801517+Hammadulhaq123@users.noreply.github.com>
2024-08-23 12:06:26 +0500

    Create: App icon added

--- src/components/authentication/SocialLogin.jsx
+++ src/components/authentication/SocialLogin.jsx
@@ -9,7 +9,7 @@ import app, {
 } from "../../firebase/firebase";
 import Cookies from "js-cookie";
 import { FaApple, FaFacebookF } from "react-icons/fa";
-import { signInWithPopup } from "firebase/auth";
+import { FacebookAuthProvider, signInWithPopup } from "firebase/auth";
 import { Google } from "../../assets/export";
 import SocialSignupModal from "./SocialSignupModal";
 import authentication from "../../api/authenticationInterceptor";
@@ -21,6 +21,50 @@ const SocialLogin = () => {
   const [googleLoading, setGoogleLoading] = useState(false);
   const [appleLoading, setAppleLoading] = useState(false);
   const [facebookLoading, setFacebookLoading] = useState(false);
+
+  const handleFacebookLogin = async () => {
+    try {
+      setFacebookLoading(true);
+      const provider = new FacebookAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      if (result) {
+        // console.log(result);
+        const token = await result?.user?.getIdToken();
+        if (token) {
+          try {
+            const email = result?.user?.email;
+            const ip = await authentication.get(
+              "https://api.ipify.org?format=json"
+            );
+            const response = await authentication.post(
+              "/auth/driverSocialSignup",
+              {
+                email: email,
+                idToken: token,
+                ip: ip.data.ip,
+              }
+            );
+
+            if (response?.data?.success) {
+              localStorage.setItem("token", response?.data?.token);
+              setIsSocialLogin(true);
+              navigate("Complete Profile", "/complete-profile");
+            }
+          } catch (error) {
+            console.log(error);
+            setError(error.response.data.message);
+          } finally {
+            setFacebookLoading(false);
+          }
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      setError("Cannot open facebook signin modal.");
+    } finally {
+      setFacebookLoading(false);
+    }
+  };
 
   const handleAppleLogin = async () => {
     try {
@@ -116,44 +160,78 @@ const SocialLogin = () => {
         aria-label="Sign in with Google"
         class="flex items-center justify-center w-full bg-white border border-button-border-light rounded-full p-1 pr-3"
       >
-        <div class="flex items-center justify-center bg-white w-9 h-9 rounded-l">
-          <img src={Google} className="w-4 " alt="" />
-        </div>
+        {googleLoading ? (
+          <div
+            class="animate-spin inline-block size-4 border-[3px] mr-1 border-current border-t-transparent text-[#EA4335] rounded-full"
+            role="status"
+            aria-label="loading"
+          >
+            <span class="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <div class="flex items-center justify-center bg-white w-9 h-9 rounded-l">
+            <img src={Google} className="w-4 " alt="" />
+          </div>
+        )}
         <span class="text-sm text-google-text-gray tracking-wider">
           Continue with Google
         </span>
       </button>
       <button
         type="button"
+        onClick={handleAppleLogin}
         aria-label="Sign in with Google"
         class="flex items-center w-full justify-center bg-white border border-button-border-light rounded-full p-1 pr-3"
       >
-        <div class="flex items-center text-gray-800 justify-center bg-white w-9 h-9 rounded-l">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            viewBox="0 0 384 512"
-            class="text-xl"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
+        {appleLoading ? (
+          <div
+            class="animate-spin inline-block size-4 border-[3px] mr-1 border-current border-t-transparent text-[#000] rounded-full"
+            role="status"
+            aria-label="loading"
           >
-            <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path>
-          </svg>
-        </div>
+            <span class="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <div class="flex items-center text-gray-800 justify-center bg-white w-9 h-9 rounded-l">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 384 512"
+              class="text-xl"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path>
+            </svg>
+          </div>
+        )}
+
         <span class="text-sm text-google-text-gray tracking-wider">
           Continue with Apple
         </span>
       </button>
       <button
         type="button"
+        onClick={handleFacebookLogin}
         aria-label="Sign in with Facebook"
         class="flex items-center justify-center bg-white border border-button-border-light rounded-full p-1 pr-3"
       >
-        <div class="flex items-center justify-center bg-white w-9 h-9 rounded-full">
-          <FaFacebookF className="text-xl text-[#1877F2]" />
-        </div>
+        {facebookLoading ? (
+          <div
+            class="animate-spin inline-block size-4 border-[3px] mr-1 border-current border-t-transparent text-[#1877F2] rounded-full"
+            role="status"
+            aria-label="loading"
+          >
+            <span class="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <div class="flex items-center justify-center bg-white w-9 h-9 rounded-full">
+            <FaFacebookF className="text-xl text-[#1877F2]" />
+          </div>
+        )}
+
         <span class="text-sm text-google-text-gray tracking-wider">
           Sign in with Facebook
         </span>
--- src/pages/onboarding/FinalSuccess.jsx
+++ src/pages/onboarding/FinalSuccess.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaCircleCheck } from "react-icons/fa6";
 
 const FinalSuccess = () => {
   return (
-    <div className="w-screen fixed top-0 left-0 h-screen flex justify-center items-center text-3xl font-bold text-green-500">
-      Success! Your Account is created successfully now you can close the web
-      browser.
+    <div className="w-screen bg-white z-[10000] fixed top-0 text-center left-0 h-screen flex flex-col gap-2 justify-center items-center text-3xl font-bold text-green-600">
+      <FaCircleCheck className="text-7xl" />
+      <span className="text-2xl leading-6">
+        Success! <br />
+        Your account has been created successfully.
+      </span>
     </div>
   );
 };
